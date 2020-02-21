7aec360dbf247d7dfb49e05145df00c27a29380b
Brandon Walderman <brwalder@microsoft.com>
2020-02-21 15:44:40 -0800

    Rename dotnet folder, add Unit Test project.

---
+++ dotnet/Edge.Test/EdgeDriverTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.SeleniumTools.Edge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Edge.Test
+{
+    [TestClass]
+    public class EdgeDriverTests
+    {
+        [TestMethod]
+        public void TestChromiumOptions()
+        {
+            var driver = new EdgeDriver(new EdgeOptions() { UseChromium = true });
+            driver.Url = "https://bing.com";
+            Assert.IsTrue(driver.Title.Contains("Bing"), "Edge Chromium navigates successfully.");
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void TestChromiumServiceWithLegacyOptions()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestChromiumServiceWithChromiumOptions()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestLegacyOptions()
+        {
+        }
+
+        [TestMethod]
+        public void TestLegacyServiceWithLegacyOptions()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestLegacyServiceWithChromiumOptions()
+        {
+
+        }
+    }
+}
---
+++ dotnet/Edge.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Edge.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Edge.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed6b9f74-7228-44cd-8f1e-b1e5329afc1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
