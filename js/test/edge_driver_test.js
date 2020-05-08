e2c0084cc9b9b6ffb0c713d80b704de729dd2a2f
Guangyue Xu <gux@microsoft.com>
2020-05-08 14:58:15 -0500

    js binding unit test -- 3

--- js/test/edge_driver_test.js
+++ js/test/edge_driver_test.js
@@ -6,21 +6,6 @@ describe('JS selenium binding tests', function() {
     this.timeout(10000);
 
     it('test default', async function(){
-        try{
-        let driver = await new Builder()
-            .forBrowser('chrome')
-            .build();
-
-        let cap = await driver.getCapabilities();
-        await assert.equal(cap.get('browserName'), 'chrome');
-        
-        driver.quit();
-        }catch(e){
-            console.log(e);
-        }
-    });
-
-    it('test default', async function(){
         let driver = await new Builder()
             .forBrowser('MicrosoftEdge')
             .build();
@@ -28,11 +13,10 @@ describe('JS selenium binding tests', function() {
         let cap = await driver.getCapabilities();
         await assert.equal(cap.get('browserName'), 'MicrosoftEdge');
         
-        driver.quit();
+        await driver.quit();
     });
 
     it('test legacy edge', async function(){
-        try{
         let options = await new edge.Options();
         await options.setEdgeChromium(false);
 
@@ -44,14 +28,10 @@ describe('JS selenium binding tests', function() {
         let cap = await driver.getCapabilities();
         await assert.equal(cap.get('browserName'), 'MicrosoftEdge');
         
-        driver.quit();
-        }catch(e){
-            console.log(e);
-        }
+        await driver.quit();
     });
 
     it('test chromium edge', async function(){
-        try{
         let options = await new edge
             .Options()
             .setEdgeChromium(true);
@@ -65,9 +45,6 @@ describe('JS selenium binding tests', function() {
         await assert.equal(cap.get('browserName'), 'msedge');
 
         await driver.quit();
-        }catch(e){
-            console.log(e);
-        }
     });
 });
 
