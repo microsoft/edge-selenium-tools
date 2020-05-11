d7c1f97026790c5dc3ec9b2c900d0b7a9c0bfc84
Brandon Walderman <brwalder@microsoft.com>
2020-05-11 10:13:44 -0700

    Fix #4 (#10)
    * Mark assembly as CLS compliant.
    
    * Increment version number.

--- dotnet/Edge.Test/Properties/AssemblyInfo.cs
+++ dotnet/Edge.Test/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -18,3 +19,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CLSCompliant(true)]
